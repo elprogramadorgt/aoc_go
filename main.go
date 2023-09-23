8f2724ff917801b4eaeed6a72de06fb00a163545
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-22 23:39:32 -0500

    ++ Testing Day03 in main

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"github.com/elprogramadorgt/aoc_go/day01"
+	//"github.com/elprogramadorgt/aoc_go/day01"
+	//"github.com/elprogramadorgt/aoc_go/day02"
+	"github.com/elprogramadorgt/aoc_go/day03"
 )
 
 func main() {
 
 	//day01.SolutionDay01Exercise01()
-	day01.SolutionDay01Exercise02()
-
-	
+	//day01.SolutionDay01Exercise02()
+	//day02.SolutionDay02Exercise01()
+	//day02.SolutionDay02Exercise02()
+	//day03.SolutionDay03Exercise01()
+	day03.SolutionDay03Exercise02()
 }
