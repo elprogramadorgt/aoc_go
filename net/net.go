9acea2d16970ad909d4a8b16fba765a42f9408b0
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-09-22 23:34:49 -0500

    ++ net module

--- net/net.go
+++ net/net.go
@@ -12,8 +12,7 @@ func GetRemoteFile(url string) ([]byte, error)  {
 
 	req, err := http.NewRequest("GET", url, nil)
 	
-	req.Header.Set("Cookie", "_ga=GA1.2.1324803828.1688699141; _ga_MHSNPJKWC7=GS1.2.1690335385.2.1.1690336637.0.0.0; _gid=GA1.2.2109103212.1690335385; session=53616c7465645f5fa358497347a4c745e807e0e5240d936cb580425af79ec84c6798175e9a2356fd29b112f6345183c86a11e5cd81f0d1a5ecf774c1af89b663")
-	
+	req.Header.Set("Cookie", "_ga=GA1.2.1324803828.1688699141; _ga_MHSNPJKWC7=GS1.2.1693963903.11.1.1693964221.0.0.0; _gid=GA1.2.1518597740.1693963902; session=53616c7465645f5fd323b23bf55e19d41f16fb784f22a86a1885df60cd910253dc8fabc44eb628ba7a41a57a405dfa413d4249d92d79fdbda7e38aff2b80cb0d")
 
 	res, err := client.Do(req)
 
