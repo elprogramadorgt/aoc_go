83cf8e14998f7a31e4a11de5b8ebe3c69137f790
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-10-17 22:42:34 -0500

    ++  day03 format code

--- day03/day03.go
+++ day03/day03.go
@@ -2,8 +2,9 @@ package day03
 
 import (
 	"fmt"
+	"slices"
 	"strings"
-	 "slices"
+
 	"github.com/elprogramadorgt/aoc_go/net"
 )
 
@@ -13,29 +14,26 @@ var lettersMap = make(map[string]int)
 
 func initConfig() []string {
 	for i, letter := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
-		lettersMap[string(letter)] = i+1
+		lettersMap[string(letter)] = i + 1
 	}
- 	body, err := net.GetRemoteFile(url)
+	body, err := net.GetRemoteFile(url)
 
 	if err != nil {
 		fmt.Println("Error on connection")
 	}
 
 	bodyStr := strings.TrimSpace(string(body))
-	
+
 	dataArr := strings.Split(bodyStr, "\n")
 
 	return dataArr
-
 
 }
 
 //letters := map[string]int{"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,
 //"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}
 
-
-
-func SolutionDay03Exercise01(){
+func SolutionDay03Exercise01() {
 	dataArr := initConfig()
 
 	var repeated []string
@@ -49,7 +47,7 @@ func SolutionDay03Exercise01(){
 			letter := string(item)
 			if slices.Contains(strings.Split(second, ""), letter) {
 				if !slices.Contains(repeatedLetter, letter) {
-					repeated = append(repeated, letter)	
+					repeated = append(repeated, letter)
 					repeatedLetter = append(repeatedLetter, letter)
 				}
 			}
@@ -62,71 +60,42 @@ func SolutionDay03Exercise01(){
 	fmt.Println(total)
 }
 
-
 func SolutionDay03Exercise02() {
 
 	dataArr := initConfig()
 	groupArr := []string{}
 	var repeatedLetter []string
 	for i, v := range dataArr {
-		
+
 		groupArr = append(groupArr, v)
-		
-        if(len(groupArr) == 3){
 
-            fmt.Println(i, "i on 3")
+		if len(groupArr) == 3 {
 
-			for _, item := range groupArr[0]{
-			    letter := string(item)
-                existsInG2 := strings.Contains(groupArr[1], letter)
-                existsInG3 := strings.Contains(groupArr[2], letter)
-                
-                if existsInG2 && existsInG3 {
-                    repeatedLetter = append(repeatedLetter, letter)
-                    
-                    break
-                }
+			fmt.Println(i, "i on 3")
+
+			for _, item := range groupArr[0] {
+				letter := string(item)
+				existsInG2 := strings.Contains(groupArr[1], letter)
+				existsInG3 := strings.Contains(groupArr[2], letter)
+
+				if existsInG2 && existsInG3 {
+					repeatedLetter = append(repeatedLetter, letter)
+
+					break
+				}
 			}
 			groupArr = nil
 		}
 
 	}
 
-    fmt.Println(repeatedLetter)
-    total := 0
-    for _, letter := range repeatedLetter {
-    
-        total += lettersMap[letter]
-    }
+	fmt.Println(repeatedLetter)
+	total := 0
+	for _, letter := range repeatedLetter {
 
-    fmt.Println(total)
-//	for i:=3; i < len(dataArr); i+=3 {
-//		for j := i; j >= i-3; j-- {
-//			fmt.Println(dataArr[j])
-//		}
-//
-//		fmt.Println("========",i)
-//	}
-		
+		total += lettersMap[letter]
+	}
 
-
+	fmt.Println(total)
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
